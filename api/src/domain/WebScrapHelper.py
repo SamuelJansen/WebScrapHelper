9b3ac8d0f2f6816a01413dbe37c5473d070b5704
Samuel Jansen <samuel.jansen@cwi.com.br>
2020-05-18 01:50:56 -0300

    feat: Helper, WebScraper and CifrasClubWebScraper

---
+++ api/src/domain/WebScrapHelper.py
@@ -0,0 +1,20 @@
+import SeleniumHelper
+
+FILE_FOLDER_LOCAL_PATH = 'repository\\file\\'
+
+KW_API = 'api'
+KW_NAME = 'name'
+KW_MAIN_URL = 'main-url'
+
+class WebScrapHelper(SeleniumHelper.SeleniumHelper):
+
+    def __init__(self,globals,**kwargs):
+        SeleniumHelper.SeleniumHelper.__init__(self,globals,**kwargs)
+        self.name = self.globals.getApiSetting(f'{KW_API}.{KW_NAME}')
+        self.mainUrl = self.globals.getApiSetting(f'{KW_API}.{KW_MAIN_URL}')
+
+    def run(self,commandList):
+        self.globals.debug(f'[{self.globals.apiName}] run() method not implemented')
+
+    def accessMainUrl(self):
+        return self.accessUrl(self.mainUrl)
